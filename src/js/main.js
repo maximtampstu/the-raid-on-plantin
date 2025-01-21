67408b85873c6286ae5f6a8893dc0cd18257756e
Maxim Tampere <maxim.tampere@student.howest.be>
2025-01-21 20:04:19 +0100

    spelling corrector and cliffhanger done

---
+++ src/js/main.js
@@ -0,0 +1,304 @@
+/*
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
+*/
+
+gsap.registerPlugin(ScrollTrigger);
+
+const navInteraction = () => {
+
+  const $navButton = document.querySelector('.phone-button');
+  const $navList = document.querySelector('.nav__list');
+  const listItems = $navList.querySelectorAll("li");
+  const $closeButton = document.querySelector('.nav__item--my-beginning button')
+  
+  $navButton.classList.remove('visually-hidden');
+  $navList.classList.add("visually-hidden");
+  
+  const openNavigation = () => {
+    $navList.classList.remove("visually-hidden");
+    $navButton.classList.add('visually-hidden');
+  }
+  
+  const closeNavigation = () => {
+    $navList.classList.add("visually-hidden");
+    $navButton.classList.remove('visually-hidden');
+  }
+  
+  $navButton.addEventListener("click", openNavigation);
+  $closeButton.addEventListener("click", closeNavigation);
+  listItems.forEach(item => {
+    item.addEventListener('click', () => {
+      const link = item.querySelector('a');
+  
+      if (link.getAttribute('href') !== '#' && link.getAttribute('href') !== "") {
+        closeNavigation();
+      }
+    });
+  });
+
+}
+
+const phoneBar = (scrolled) => {
+  const canvas = document.querySelector(".phone-button__progress");
+  const ctx = canvas.getContext("2d");
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.beginPath();
+  ctx.arc(28, 28, 25, 1.5 * Math.PI, 1.5 * Math.PI + (2 * Math.PI * (scrolled / 100)));
+  ctx.lineWidth = 12;
+  ctx.strokeStyle = "#E8E0D3";
+  ctx.stroke();
+
+}
+
+const progressBar = () => {
+  const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const scrolled = (winScroll / height) * 100;
+
+  phoneBar(scrolled)
+}
+
+const nav = () => {
+  navInteraction();
+  document.addEventListener("scroll", progressBar);
+}
+
+const appearEffect = (itemClass, showPercent) => {
+  const appearItems = document.querySelectorAll(itemClass);
+  appearItems.forEach(item => {
+    gsap.set(item, { opacity: 0 })
+    gsap.to(item, {
+      opacity: 1,
+      duration: 2,
+      scrollTrigger: {
+        trigger: item,
+        start: `top ${showPercent}%`,
+      }
+    });
+  });
+}
+
+const letterMother = () => {
+
+  const $letterButton = document.querySelector('.letter-button');
+  const $letter = document.querySelector('.letter-container');
+  const $closeButton = document.querySelector('.letter-container button')
+
+  const $navButton = document.querySelector('.phone-button');
+
+  $letterButton.classList.remove('visually-hidden');
+  $letter.classList.add("visually-hidden");
+
+  const openNavigation = () => {
+    $letter.classList.remove("visually-hidden");
+    $navButton.classList.add('visually-hidden');
+  }
+
+  const closeNavigation = () => {
+    $letter.classList.add("visually-hidden");
+    $navButton.classList.remove('visually-hidden');
+  }
+
+  $letterButton.addEventListener("click", openNavigation);
+  $closeButton.addEventListener("click", closeNavigation);
+
+}
+
+
+
+
+const binding = () => {
+  const topList = document.querySelector(".bookbind__list--top ul");
+  console.log(Math.floor(topList.offsetWidth / 80))
+  for (let i = 0; i < Math.floor(topList.offsetWidth / 80); i++) {
+    const circle = document.createElement("li")
+    circle.innerHTML = `<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="16.3334" cy="16" r="16" fill="#232321"/>
+      </svg>`
+    topList.appendChild(circle);
+  }
+}
+
+
+
+const moving = (e) => {
+  element.style.top = `${e.pageY - element.offsetWidth / 2}px`;
+  element.style.left = `${e.pageX - element.offsetWidth / 2}px`;
+}
+const element = document.createElement("img");
+
+const test = (e) => {
+  element.src = "src/image/blueprint-living.png"
+  element.style.position = "absolute";
+  element.style.width = "5.75rem";
+  element.style.top = `${e.pageY - element.offsetWidth / 2}px`;
+  element.style.left = `${e.pageX - element.offsetWidth / 2}px`;
+  
+  document.querySelector(".blueprint__game").appendChild(element)
+}
+
+const upl = () => {
+  document.querySelector(".blueprint__game").removeChild(element)
+}
+
+const blueprint = () => {
+  document.querySelector(".blueprint__living--item").addEventListener("mousedown", test)
+  document.addEventListener("mousemove", moving(element))
+  document.addEventListener("mouseup", upl)
+}
+
+const startCorrector = () => {
+  const $screen1 = document.querySelector(".corrector__screen--1");
+  const $screen2 = document.querySelector(".corrector__screen--2");
+
+  $screen2.classList.remove("visually-hidden");
+  $screen1.classList.add('visually-hidden');
+}
+
+const againCorrector = () => {
+  const $screen2 = document.querySelector(".corrector__screen--2");
+  const $screen3 = document.querySelector(".corrector__screen--3");
+
+  document.querySelector(".corrector__points").textContent = 0;
+  document.querySelectorAll(".corrector__mistake").forEach(mistake => {
+    mistake.style.color = "var(--color-black)";
+  });
+
+  $screen2.classList.remove("visually-hidden");
+  $screen3.classList.add('visually-hidden');
+}
+
+
+const corrector = () => {
+  const $screen1 = document.querySelector(".corrector__screen--1");
+  $screen1.classList.remove('visually-hidden');
+
+  const $screen2 = document.querySelector(".corrector__screen--2");
+  $screen2.classList.add('visually-hidden');
+
+  const $screen3 = document.querySelector(".corrector__screen--3");
+  $screen3.classList.add('visually-hidden');
+
+  document.querySelector(".corrector__start").addEventListener("click", startCorrector)
+
+  document.querySelectorAll(".corrector__mistake").forEach(mistake => {
+    mistake.addEventListener("click", () => {
+      if(mistake.style.color != "var(--color-red)"){
+        mistake.style.color = "var(--color-red)";
+
+        const scoreTag = document.querySelector(".corrector__points")
+        let score = parseInt(scoreTag.textContent, 10);
+        score++
+        scoreTag.textContent = score;
+
+        if(score >= 5){
+          $screen3.classList.remove("visually-hidden");
+          $screen2.classList.add('visually-hidden');
+        }
+      }
+    })
+  });
+  document.querySelector(".corrector__again").addEventListener("click", againCorrector)
+
+
+}
+
+const cliffMask = (maskClass, maskSize, maskContainer, maskStart, maskEnd) => {
+  const mask = document.querySelector(maskClass);
+
+  ScrollTrigger.create({
+    trigger: maskContainer, 
+    start: `top ${maskStart}`, 
+    end: `bottom ${maskEnd}`, 
+    onEnter: () => mask.classList.add("mask__active"),
+    onLeave: () => mask.classList.remove("mask__active"),
+    onEnterBack: () => mask.classList.add("mask__active"),
+    onLeaveBack: () => mask.classList.remove("mask__active"),
+  });
+  
+  gsap.to(mask, {
+    maskImage: `radial-gradient(circle, transparent ${maskSize}%, black ${maskSize + 2}%)`,
+    webkitMaskImage: `radial-gradient(circle, transparent ${maskSize}%, black ${maskSize + 2}%)`,
+    scrollTrigger: {
+      trigger: maskContainer,
+      start: `top ${maskStart}`,
+      end: `bottom ${maskEnd}`,
+      scrub: true,
+      //markers: true,
+    },
+  });
+}
+
+const cliffSound = (soundContainer, soundPath) => {
+  const sound = new Audio(`public/sounds/${soundPath}`);
+
+  ScrollTrigger.create({
+    trigger: soundContainer, 
+    start: "top 40%",
+    onEnter: () => {
+      sound.play(); 
+    },
+  });
+}
+
+const cliff = () => {
+  cliffMask(".cliff__mask--1", 50, ".cliff__list--1", "bottom", "top")
+  cliffMask(".cliff__mask--2", 15, ".cliff__list--2", "bottom", "top")
+  cliffMask(".cliff__mask--3", -5, ".cliff__list--3", "bottom", "top")
+
+  cliffSound(".cliff__text-4", "knocking.mp3")
+  cliffSound(".cliff__text-6", "invation.wav")
+  cliffSound(".cliff__text-8", "steps.mp3")
+
+  gsap.set(".cta__text", { x: window.innerWidth, opacity: 0})
+  gsap.to(".cta__text", {
+    x: 0,
+    opacity: 1,
+    scrollTrigger: {
+      trigger: ".cta",
+      scrub: true,
+      //pin: ".cta__text",
+      start: "top 75%",
+      end: "top 25%",
+      //markers: true,
+    },
+  });
+}
+
+const init = () => {
+  nav();
+  letterMother();
+  appearEffect(".appear", 85);
+  appearEffect(".appear-late", 40);
+  //binding();
+  //blueprint();
+  corrector();
+  cliff();
+}
+
+init();
